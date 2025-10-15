8c0f72684d8dbc7d1e5e4d5351a0ec36faf71775
Luis.Lozano <>
2025-10-14 23:02:26 -0500

    create page theraty_service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { TherapyServicesComponent } from './pages/therapy-services/therapy-services.component';
 
 
 
 export const routes: Routes = [{
     path: '', component: HomeComponent, title: 'Inicio'
 },
-{path: 'Quien_Soy', component: AboutMeComponent, title: 'Quién Soy'}
+{path: 'Quien_Soy', component: AboutMeComponent, title: 'Quién Soy'},
+{path: 'Servicios', component: TherapyServicesComponent, title: 'Servicios'}
 ];
