6f6a06282d2075d0c6ec909704b98588a9e96bcc
Luis.Lozano <>
2025-10-20 20:34:48 -0500

    fixes links

--- src/app/pages/therapy-services/therapy-services.component.ts
+++ src/app/pages/therapy-services/therapy-services.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModalCalendarComponent } from '../../components/modal-calendar/modal-calendar.component';
 import { ModalInsertComponent } from '../../components/modal-insert/modal-insert.component';
 
 
 @Component({
   selector: 'app-therapy-services',
   standalone: true,
-  imports: [ModalCalendarComponent, CommonModule,ModalInsertComponent],
+  imports: [CommonModule,ModalInsertComponent],
   templateUrl: './therapy-services.component.html',
   styleUrl: './therapy-services.component.css'
 })
